05b6eb9a87d5802c1b726b4732d3c28906c12fe9
dmitriirove <dmitriiv@roveconcepts.com>
2024-01-15 20:31:22 -0800

    feat: AR

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import { Controllers, Hands, VRButton, XR } from '@react-three/xr'
-import './styles.css'
-import { Canvas } from '@react-three/fiber'
+// import { createRoot } from 'react-dom/client'
+// import React from 'react'
+// import { Controllers, Hands, VRButton, XR } from '@react-three/xr'
+// import './styles.css'
+// import { Canvas } from '@react-three/fiber'
 
-// Oculus Browser with #webxr-hands flag enabled
-function App() {
-  return (
-    <>
-      <VRButton />
-      <Canvas>
-        <XR>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Controllers />
-          <Hands />
-        </XR>
-      </Canvas>
-    </>
-  )
-}
+// // Oculus Browser with #webxr-hands flag enabled
+// function App() {
+//   return (
+//     <>
+//       <VRButton />
+//       <Canvas>
+//         <XR>
+//           <ambientLight />
+//           <pointLight position={[10, 10, 10]} />
+//           <Controllers />
+//           <Hands />
+//         </XR>
+//       </Canvas>
+//     </>
+//   )
+// }
 
-//@ts-ignore
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+// //@ts-ignore
+// const root = createRoot(document.getElementById('root'))
+// root.render(<App />)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react'
-import { Controllers, Hands, VRButton, XR } from '@react-three/xr'
+import React, { Suspense, useState } from 'react'
+import { Controllers, Hands, ARButton, XR, Interactive } from '@react-three/xr'
+import { Text } from '@react-three/drei'
 import './index.css'
 import { Canvas } from '@react-three/fiber'
+
+
+function Box({ color, size, scale, children, ...rest }) {
+  return (
+    <mesh scale={scale} {...rest}>
+      <boxGeometry args={size} />
+      <meshPhongMaterial color={color} />
+      {children}
+    </mesh>
+  )
+}
+
+function Button(props) {
+  const [hover, setHover] = useState(false)
+  const [color, setColor] = useState('blue')
+
+  const onSelect = () => {
+    setColor((Math.random() * 0xffffff) | 0)
+  }
+
+  return (
+    <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
+      <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
+        <Suspense fallback={null}>
+          <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+            Hello react-xr!
+          </Text>
+        </Suspense>
+      </Box>
+    </Interactive>
+  )
+}
 
 function App() {
   return (
     <>
-      <VRButton />
+      <ARButton />
       <Canvas>
-        <XR>
+        <XR referenceSpace="local">
           <ambientLight />
-          <pointLight position={[0, 0, 0]} intensity={10}/>
+          <pointLight position={[0, 0, 0]} intensity={10} />
+          <Button position={[0, 0.1, -0.2]} />
           <Controllers />
           <Hands />
         </XR>
